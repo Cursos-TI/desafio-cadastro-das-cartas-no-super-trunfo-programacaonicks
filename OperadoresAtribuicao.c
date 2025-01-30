2bd80011559f6a88f721d4f2506dd5c49eb11e3c
programacaonicks <nicolelevitoria@gmail.com>
2025-01-30 22:38:28 +0000

    demonstracao do github

---
+++ OperadoresAtribuicao.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    /*
+    Incremento (++)
+    Pre incremneto ++a
+    Pos incremento a++
+    Decremento (--)
+    Pre decremento --a
+    Pos incremento a--
+    */
+
+   int numero1 = 1;
+
+    printf("Antes incremento: %d\n", numero1);
+   numero1++;
+    printf("Depois incremento: %d\n", numero1);
+
+
+
+}
